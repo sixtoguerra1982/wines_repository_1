454d35a582862dbf649bebcfef3afe024d56bb51
Sixto Guerra <sixto.guerra1982@gmail.com>
2021-03-20 15:56:43 -0300

    alphabetical order strain OK

--- app/models/wine_strain.rb
+++ app/models/wine_strain.rb
@@ -2,4 +2,5 @@ class WineStrain < ApplicationRecord
   belongs_to :wine
   belongs_to :strain
   accepts_nested_attributes_for :strain, allow_destroy: true
+  scope :order_by_strain_name, -> { joins(:strain).order('strains.name') }
 end
